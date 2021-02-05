8495359eab7596c6d90ca4dacd35bb4794dafbeb
Nuno Freitas <nunofreitas@gmail.com>
2021-02-05 11:06:48 +0000

    chore: setup commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+
+  ignores: [
+    (message) => {
+      if (!message) {
+        return false;
+      }
+
+      const processed = message.trim().toLowerCase();
+
+      // Exclude the following commit messages from linting.
+      return (
+        processed === "initial commit" ||
+        processed.startsWith("chore(release):")
+      );
+    },
+  ],
+};
