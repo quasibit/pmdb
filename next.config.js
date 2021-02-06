7d75d3f0939f2279571254a361ec1fecbde3f97d
Nuno Freitas <nunofreitas@gmail.com>
2021-02-06 16:37:17 +0000

    build(nextjs): enable react strict mode

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 const withLinaria = require("next-linaria");
 
-module.exports = withLinaria();
+module.exports = withLinaria({
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  reactStrictMode: true,
+
+  images: {
+    domains: process.env.IMAGES_DOMAIN ? [process.env.IMAGES_DOMAIN] : [],
+  },
+});
